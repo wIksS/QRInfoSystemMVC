1d07ae07b95b8d6f3d45d0bb3e9a841ab833368e
TCVD <viktordakov97@gmail.com>
2015-04-01 12:08:35 +0300

    Added Infrastructure folder and classes to the project.

---
+++ QRInfoSystem/Infrastructure/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QRInfoSystem.Infrastructure
+{
+    public static class Constants
+    {
+        public static string Url = "qrinfo.apphb.com";
+    }
+}
---
+++ QRInfoSystem/Infrastructure/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+
+namespace QRInfoSystem.Infrastructure
+{
+    public static class EmailSender
+    {
+        public static void Send(ICollection<string> users, string body,string subject)
+        {
+            SmtpClient client = new SmtpClient();   
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("qrinfosystem@gmail.com", "zxcvbasdfg");
+
+            MailMessage mm = new MailMessage();
+            mm.From = new MailAddress("qrinfosystem@gmail.com");
+            foreach (var email in users)
+            {
+                mm.To.Add(new MailAddress(email));   
+            }
+
+            mm.BodyEncoding = UTF8Encoding.UTF8;
+            mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            mm.Body = body;
+            mm.Subject = subject;
+            mm.IsBodyHtml = true;
+
+            client.Send(mm);
+        }
+    }
+}
